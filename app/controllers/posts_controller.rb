d2dea33ddcc158760e45b21989a7f41db7948cf3
Jasterix C <loune.calixte@gmail.com>
2019-06-02 03:49:06 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,11 +18,12 @@ class PostsController < ApplicationController
 	  redirect_to post_path(@post)
   end
  
-def update
-  @post = Post.find(params[:id])
-  @post.update(post_params)
-  redirect_to post_path(@post)
-end
+  def update
+	  @post = Post.find(params[:id])
+	  @post.update(params["post"])
+	  @post.update(post_params(:title))
+	  redirect_to post_path(@post)
+	end
  
 private
  
